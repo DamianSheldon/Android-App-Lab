d3aa08ce9b8549a6c088434de201eb99526dccca
Meiliang Dong <dongmeilianghy@sina.com>
2017-05-10 22:58:01 +0800

    Added manage network usage.

--- app/src/main/java/com/example/helloworld/network/NetworkActivity.java
+++ app/src/main/java/com/example/helloworld/network/NetworkActivity.java
@@ -1,15 +1,25 @@
 package com.example.helloworld.network;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.helloworld.BaseActivity;
 import com.example.helloworld.R;
+
+import java.util.Random;
 
 public class NetworkActivity extends BaseActivity implements DownloadCallback {
 
@@ -25,6 +35,24 @@ public class NetworkActivity extends BaseActivity implements DownloadCallback {
     // downloads with consecutive button clicks.
     private boolean mDownloading = false;
 
+    public static final String WIFI = "Wi-Fi";
+    public static final String ANY = "Any";
+    private static final String URL = "http://stackoverflow.com/feeds/tag?tagnames=android&sort=newest";
+    private static final String TAG = "NetworkActivity";
+
+    // Whether there is a Wi-Fi connection.
+    private static boolean wifiConnected = false;
+    // Whether there is a mobile connection.
+    private static boolean mobileConnected = false;
+    // Whether the display should be refreshed.
+    public static boolean refreshDisplay = true;
+
+    // The user's current network preference setting.
+    public static String sPref = null;
+
+    // The BroadcastReceiver that tracks network connectivity changes.
+    private NetworkReceiver receiver = new NetworkReceiver();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +60,60 @@ public class NetworkActivity extends BaseActivity implements DownloadCallback {
 
         mDataText = (TextView) findViewById(R.id.data_text);
         mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://cn.bing.com");
+
+        // Registers BroadcastReceiver to track network connection changes.
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        receiver = new NetworkReceiver();
+        this.registerReceiver(receiver, filter);
+    }
+
+    // Refreshes the display if the network connection and the
+    // pref settings allow it.
+
+    @Override
+    public void onStart () {
+        super.onStart();
+
+        // Gets the user's network preference settings
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Retrieves a string value for the preferences. The second parameter
+        // is the default value to use if a preference value is not found.
+        sPref = sharedPrefs.getString("listPref", "Wi-Fi");
+
+        updateConnectedFlags();
+
+        if(refreshDisplay){
+            loadPage();
+        }
+    }
+
+    // Checks the network connection and sets the wifiConnected and mobileConnected
+    // variables accordingly.
+    public void updateConnectedFlags() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+        if (activeInfo != null && activeInfo.isConnected()) {
+            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+        } else {
+            wifiConnected = false;
+            mobileConnected = false;
+        }
+    }
+
+    // Uses AsyncTask subclass to download the XML feed from stackoverflow.com.
+    public void loadPage() {
+        if (((sPref.equals(ANY)) && (wifiConnected || mobileConnected))
+                || ((sPref.equals(WIFI)) && (wifiConnected))) {
+            // AsyncTask subclass
+            new DownloadXmlTask().execute(URL);
+        }
+        else {
+            showErrorPage();
+        }
     }
 
     @Override
@@ -105,6 +187,69 @@ public class NetworkActivity extends BaseActivity implements DownloadCallback {
                 break;
             case Progress.PROCESS_INPUT_STREAM_SUCCESS:
                 break;
+        }
+    }
+
+    private void showErrorPage() {
+        Toast.makeText(this, "Network connectivity unavailable!", Toast.LENGTH_LONG).show();
+    }
+
+    public class NetworkReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager conn =  (ConnectivityManager)
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = conn.getActiveNetworkInfo();
+
+            // Checks the user prefs and the network connection. Based on the result, decides whether
+            // to refresh the display or keep the current display.
+            // If the userpref is Wi-Fi only, checks to see if the device has a Wi-Fi connection.
+            if (WIFI.equals(sPref) && networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                // If device has its Wi-Fi connection, sets refreshDisplay
+                // to true. This causes the display to be refreshed when the user
+                // returns to the app.
+                refreshDisplay = true;
+                Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
+
+                // If the setting is ANY network and there is a network connection
+                // (which by process of elimination would be mobile), sets refreshDisplay to true.
+            } else if (ANY.equals(sPref) && networkInfo != null) {
+                refreshDisplay = true;
+
+                // Otherwise, the app can't download content--either because there is no network
+                // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
+                // is no Wi-Fi connection.
+                // Sets refreshDisplay to false.
+            } else {
+                refreshDisplay = false;
+                Toast.makeText(context, R.string.lost_connection, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class DownloadXmlTask extends AsyncTask<String, Integer, Long> {
+        protected Long doInBackground(String... urls) {
+            int count = urls.length;
+            long totalSize = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++) {
+//                totalSize += Downloader.downloadFile(urls[i]);
+                totalSize += random.nextInt();
+                publishProgress((int) ((i / (float) count) * 100));
+                // Escape early if cancel() is called
+                if (isCancelled()) break;
+            }
+            return totalSize;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            Log.d(TAG, "onProgressUpdate" + String.valueOf(progress));
+        }
+
+        protected void onPostExecute(Long result) {
+            Log.d(TAG, "Downloaded " + result + " bytes");
         }
     }
 }
